7952fa4138732016a76ac5a56af22a9382304ea3
Gabriel Tavares <gabrielctavares.dev@gmail.com>
2024-09-06 22:38:24 +0000

    Criado novo algoritmo

---
+++ code-atividades/ex50.py
@@ -0,0 +1,13 @@
+#Criando algoritmo que faz uso de tuplas
+# Definindo variáveis
+nome = "João"
+idade = 25
+altura = 1.75
+
+# Criando uma tupla
+informacoes = (nome, idade, altura)
+
+# Acessando elementos da tupla
+print("Nome:", informacoes[0])
+print("Idade:", informacoes[1])
+print("Altura:", informacoes[2])
